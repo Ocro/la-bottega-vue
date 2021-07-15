89f3b50ce1156d50fad35d53f23eb2195418fe6e
David Miserez <david.miserez@altux.ch>
2021-07-15 14:46:30 +0200

    ajoute le composant pour le menu principal

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,12 @@
+// https://tailwindui.com/components/marketing/elements/headers
+<template>
+  <div>Salut</div>
+</template>
+
+<script>
+export default {
+  name: "MainMenu",
+};
+</script>
+
+<style scoped></style>
